181ab65a2ce5fa860b7394cd935f1c2fbc371ef3
sharki <49736448+subtosharki@users.noreply.github.com>
2021-10-18 16:50:45 -0400

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const { upload } = require('./functions/Upload');
-const { photoData } = require('./functions/PhotoData');
-const { translate } = require('./functions/Translate');
-
-module.exports = {
-    upload,
-    photoData,
-    translate,
-};
