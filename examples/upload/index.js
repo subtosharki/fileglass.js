61c423e734df00d4997f61439ab46d7d80b8d87e
sharki <49736448+subtosharki@users.noreply.github.com>
2021-10-18 16:51:06 -0400

    Delete examples directory

--- examples/upload/index.js
+++
@@ -1,4 +0,0 @@
-const fileglass = require('fileglass.js');
-
-//Call The Upload Function From The Wrapper, Followed With The Parameters
-fileglass.upload('sdfsdgdhnjiaiuhifuwabe', 'C:/Users/camyw/Pictures/logo.png');
